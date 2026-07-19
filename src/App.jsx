4ff1c8704620dd853d0d555f5ae9cc54afd48b9f
unknown <mahmudur.salman@gmail.com>
2026-07-20 00:10:49 +0600

    links fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Hero } from "@/sections/Hero";
 import { About } from "@/sections/About";
 import { Projects } from "@/sections/Projects";
 import { Testimonials } from "@/sections/Testimonials";
-import { Contact } from "@/sections/Contact";
 import { Footer } from "./layout/Footer";
 import { Education } from "./sections/Educaiton";
 import { Experience } from "./sections/Experience";
 import Certifications from "./sections/Certifications";
 import ArtGallery from "./sections/ArtGallery";
+import { Contact } from "./sections/Contact";
 
 function App() {
   return (
